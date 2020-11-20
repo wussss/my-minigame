00cf11265f51f77c3824344bed32f76ba6f36188
qiyuan.tang01 <qiyuan.tang01@liulishuo.com>
2020-11-20 11:27:18 +0800

    feat: finish

--- game.js
+++ game.js
@@ -58,23 +58,44 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var _this = this;
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var canvas = wx.createCanvas(); //创建画布
 var ctx = canvas.getContext("2d"); //创建一个2d 画笔
 var _a = wx.getSystemInfoSync(), windowWidth = _a.windowWidth, windowHeight = _a.windowHeight;
-function createImage(name) {
+function createImage(info) {
     var img = wx.createImage();
-    img.src = "./images/" + name + ".png";
+    img.src = "./images/" + info + ".png";
     return new Promise(function (resolve, reject) {
         img.onload = function () {
             resolve(img);
         };
     });
 }
+var initData = { x: 0, y: 0, width: 0, height: 0 };
+var getData = function () {
+    var x = Math.random() * windowWidth - 50;
+    return {
+        bg: __assign(__assign({}, initData), { width: windowWidth, height: windowHeight, step: 0 }),
+        dog: __assign(__assign({}, initData), { x: windowWidth / 2 - 50, y: windowHeight - 100, step: 0 }),
+        food1: __assign(__assign({}, initData), { x: Math.random() * windowWidth - 50, step: 1 }),
+        shit: __assign(__assign({}, initData), { x: Math.random() * windowWidth - 50, step: 1 })
+    };
+};
+var Kind;
+(function (Kind) {
+    Kind[Kind["Negative"] = 0] = "Negative";
+    Kind[Kind["Positive"] = 1] = "Positive";
+})(Kind || (Kind = {}));
 var Entity = /** @class */ (function () {
-    function Entity(name, info) {
-        this.name = name;
+    function Entity(info, feature) {
         this.info = info;
+        this.feature = feature;
     }
     Entity.prototype.init = function () {
         return __awaiter(this, void 0, void 0, function () {
@@ -83,7 +104,7 @@ var Entity = /** @class */ (function () {
                 switch (_b.label) {
                     case 0:
                         _a = this;
-                        return [4 /*yield*/, createImage(this.name)];
+                        return [4 /*yield*/, createImage(this.info.name)];
                     case 1:
                         _a.image = _b.sent();
                         return [2 /*return*/, this];
@@ -91,22 +112,26 @@ var Entity = /** @class */ (function () {
             });
         });
     };
-    Entity.prototype.render = function (info) {
-        if (info === void 0) { info = this.info; }
+    Entity.prototype.render = function (context) {
+        if (context === void 0) { context = ctx; }
         return __awaiter(this, void 0, void 0, function () {
-            var x, y, width, height;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
+            var _a, x, y, width, height;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
                     case 0:
-                        console.log(this.name, " render");
                         if (!this.image) return [3 /*break*/, 2];
                         return [4 /*yield*/, this.init()];
                     case 1:
-                        _a.sent();
-                        _a.label = 2;
+                        _b.sent();
+                        _b.label = 2;
                     case 2:
-                        x = info.x, y = info.y, width = info.width, height = info.height;
-                        ctx.drawImage(this.image, x, y);
+                        _a = this.feature, x = _a.x, y = _a.y, width = _a.width, height = _a.height;
+                        if (width && height) {
+                            ctx.drawImage(this.image, x, y, width, height);
+                        }
+                        else {
+                            ctx.drawImage(this.image, x, y);
+                        }
                         return [2 /*return*/, this];
                 }
             });
@@ -116,11 +141,10 @@ var Entity = /** @class */ (function () {
 }());
 var MovedEntity = /** @class */ (function (_super) {
     __extends(MovedEntity, _super);
-    function MovedEntity(name, info, step) {
-        if (step === void 0) { step = 0; }
-        var _this = _super.call(this, name, info) || this;
-        _this.name = name;
+    function MovedEntity(info, feature, step) {
+        var _this = _super.call(this, info, feature) || this;
         _this.info = info;
+        _this.feature = feature;
         _this.step = step;
         return _this;
     }
@@ -129,9 +153,14 @@ var MovedEntity = /** @class */ (function (_super) {
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
-                        console.log(this.name, " move");
-                        this.info.y += this.step;
-                        return [4 /*yield*/, this.render(this.info)];
+                        this.feature.y += this.step;
+                        if (this.feature.y > windowHeight) {
+                            this.feature.y = 0;
+                            if (this.step < 3) {
+                                this.step += 1;
+                            }
+                        }
+                        return [4 /*yield*/, this.render(this.feature)];
                     case 1:
                         _a.sent();
                         return [2 /*return*/, this];
@@ -141,35 +170,130 @@ var MovedEntity = /** @class */ (function (_super) {
     };
     return MovedEntity;
 }(Entity));
-var initData = { x: 0, y: 0, width: 0, height: 0 };
-var data = {
-    // bg: { ...initData, width: windowWidth, height: windowHeight },
-    dog: __assign(__assign({}, initData), { x: windowWidth / 2 - 50, y: windowHeight - 100 }),
-    food1: __assign(__assign({}, initData), { x: windowWidth / 2 - 50 })
-};
-console.log("entry");
-Promise.all(Object.keys(data).map(function (key) { return __awaiter(_this, void 0, void 0, function () { return __generator(this, function (_a) {
-    switch (_a.label) {
-        case 0: return [4 /*yield*/, new MovedEntity(key, data[key]).init()];
-        case 1: return [2 /*return*/, _a.sent()];
+var WORLD = { score: 0 };
+var AutoMovedEntity = /** @class */ (function (_super) {
+    __extends(AutoMovedEntity, _super);
+    function AutoMovedEntity(info, feature, step, kind) {
+        var _this = _super.call(this, info, feature, step) || this;
+        _this.info = info;
+        _this.feature = feature;
+        _this.step = step;
+        _this.kind = kind;
+        return _this;
     }
-}); }); })).then(function (entities) {
-    console.log("start");
-    function render() {
-        console.log("render");
-        // ctx.clearRect(0, 0, windowWidth, windowHeight); //清除整个画布
-        entities.forEach(function (item) { return item.move(); });
-        requestAnimationFrame(render); //自动重绘
-    }
-    render();
-});
-// function renderEach(x, y, x1, y1) {
-//   ctx.clearRect(0, 0, windowWidth, windowHeight); //清除整个画布
-//   ctx.drawImage(bg, 0, 0, windowWidth, windowHeight); //背景图
-//   ctx.drawImage(dog, x, y); //狗狗
-//   ctx.drawImage(food, x1, y1); //食物
-// }
-// wx.onTouchMove((res) => {
-//   dX = res.changedTouches[0].clientX;
-//   dY = res.changedTouches[0].clientY;
-// }); //狗狗跟随触摸点移动
+    AutoMovedEntity.prototype.move = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        this.feature.y += this.step;
+                        if (this.feature.y > windowHeight) {
+                            this.feature.y = 0;
+                            if (this.step < 3) {
+                                this.step += 1;
+                            }
+                        }
+                        return [4 /*yield*/, this.render(this.feature)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/, this];
+                }
+            });
+        });
+    };
+    AutoMovedEntity.prototype.match = function (target) {
+        var x = target.x, y = target.y, width = target.width, height = target.height;
+        if (Math.abs(this.feature.x - x) < 5 && Math.abs(this.feature.y - y) < 20) {
+            this.hit();
+            var food1 = getData().food1;
+            this.feature = __assign(__assign({}, this.feature), food1);
+        }
+    };
+    AutoMovedEntity.prototype.hit = function () {
+        // 得分
+        if (this.kind === Kind.Positive) {
+            console.log("得分");
+            WORLD.score++;
+        }
+        else {
+            // 扣分
+            console.log("扣分");
+            WORLD.score -= 10;
+        }
+    };
+    return AutoMovedEntity;
+}(MovedEntity));
+function run() {
+    return __awaiter(this, void 0, void 0, function () {
+        function render() {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    list.map(function (item) { return item.match(dog.feature); });
+                    Promise.all(__spreadArrays([bg, dog], list).map(function (item) { return item.move(); }));
+                    ctx.font = "20px Georgia";
+                    ctx.fillText(String(WORLD.score), 10, 20);
+                    requestAnimationFrame(render);
+                    return [2 /*return*/];
+                });
+            });
+        }
+        var data, _a, bg, dog, list;
+        var _this = this;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    data = getData();
+                    return [4 /*yield*/, Promise.all(Object.keys({ bg: data.bg, dog: data.dog }).map(function (key) { return __awaiter(_this, void 0, void 0, function () { return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, new MovedEntity({ name: key }, data[key], 0).init()];
+                                case 1: return [2 /*return*/, _a.sent()];
+                            }
+                        }); }); }))];
+                case 1:
+                    _a = _b.sent(), bg = _a[0], dog = _a[1];
+                    list = [];
+                    setInterval(function () { return __awaiter(_this, void 0, void 0, function () {
+                        var data, _a, _b;
+                        return __generator(this, function (_c) {
+                            switch (_c.label) {
+                                case 0:
+                                    if (list.length > 10) {
+                                        return [2 /*return*/];
+                                    }
+                                    data = getData();
+                                    _b = (_a = list).push;
+                                    return [4 /*yield*/, new AutoMovedEntity({ name: "shit" }, data.shit, data.shit.step, Kind.Negative).init()];
+                                case 1:
+                                    _b.apply(_a, [_c.sent()]);
+                                    return [2 /*return*/];
+                            }
+                        });
+                    }); }, 1000 * 10);
+                    setInterval(function () { return __awaiter(_this, void 0, void 0, function () {
+                        var data, _a, _b;
+                        return __generator(this, function (_c) {
+                            switch (_c.label) {
+                                case 0:
+                                    if (list.length > 10) {
+                                        return [2 /*return*/];
+                                    }
+                                    data = getData();
+                                    _b = (_a = list).push;
+                                    return [4 /*yield*/, new AutoMovedEntity({ name: "food1" }, data.food1, data.food1.step, Kind.Positive).init()];
+                                case 1:
+                                    _b.apply(_a, [_c.sent()]);
+                                    return [2 /*return*/];
+                            }
+                        });
+                    }); }, 1000 * 1);
+                    render();
+                    wx.onTouchMove(function (res) {
+                        dog.feature.x = res.changedTouches[0].clientX - 50;
+                        dog.feature.y = res.changedTouches[0].clientY - 50;
+                    }); //狗狗跟随触摸点移动
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+run();
--- game.ts
+++ game.ts
@@ -1,15 +1,51 @@
 const canvas = wx.createCanvas(); //创建画布
 const ctx = canvas.getContext("2d"); //创建一个2d 画笔
+
 const { windowWidth, windowHeight } = wx.getSystemInfoSync();
 
-function createImage(name: string) {
+function createImage(info: string) {
   const img = wx.createImage();
-  img.src = `./images/${name}.png`;
+  img.src = `./images/${info}.png`;
   return new Promise((resolve, reject) => {
     img.onload = () => {
       resolve(img);
     };
   });
+}
+
+const initData = { x: 0, y: 0, width: 0, height: 0 };
+
+const getData: () => { [props: string]: any } = () => {
+  const x = Math.random() * windowWidth - 50;
+
+  return {
+    bg: { ...initData, width: windowWidth, height: windowHeight, step: 0 },
+    dog: {
+      ...initData,
+      x: windowWidth / 2 - 50,
+      y: windowHeight - 100,
+      step: 0,
+    },
+    food1: {
+      ...initData,
+      x: Math.random() * windowWidth - 50,
+      step: 1,
+    },
+    shit: {
+      ...initData,
+      x: Math.random() * windowWidth - 50,
+      step: 1,
+    },
+  };
+};
+
+enum Kind {
+  Negative,
+  Positive,
+}
+
+interface Info {
+  name: string;
 }
 
 interface Point {
@@ -21,78 +57,165 @@ interface Shape {
   height?: number;
 }
 
+interface Context {
+  ctx?: any;
+}
+
 class Entity {
   image: any;
 
-  constructor(public name: string, public info: Point & Shape) {}
+  constructor(public info: Info, public feature: Point & Shape) {}
 
   async init() {
-    this.image = await createImage(this.name);
+    this.image = await createImage(this.info.name);
     return this;
   }
 
-  async render(info: Point & Shape = this.info) {
-    console.log(this.name, " render");
+  async render(context: any = ctx) {
     if (this.image) {
       await this.init();
     }
-    const { x, y, width, height } = info;
-    ctx.drawImage(this.image, x, y);
+    const { x, y, width, height } = this.feature;
+    if (width && height) {
+      ctx.drawImage(this.image, x, y, width, height);
+    } else {
+      ctx.drawImage(this.image, x, y);
+    }
+
     return this;
   }
 }
 
 class MovedEntity extends Entity {
   constructor(
-    public name: string,
-    public info: Point & Shape,
-    public step: number = 0
+    public info: Info,
+    public feature: Point & Shape,
+    public step: number
   ) {
-    super(name, info);
+    super(info, feature);
   }
 
   async move() {
-    console.log(this.name, " move");
-    this.info.y += this.step;
-    await this.render(this.info);
+    this.feature.y += this.step;
+
+    if (this.feature.y > windowHeight) {
+      this.feature.y = 0;
+
+      if (this.step < 3) {
+        this.step += 1;
+      }
+    }
+
+    await this.render(this.feature);
     return this;
   }
 }
 
-const initData = { x: 0, y: 0, width: 0, height: 0 };
+const WORLD = { score: 0 };
 
-const data: { [props: string]: Point & Shape } = {
-  // bg: { ...initData, width: windowWidth, height: windowHeight },
-  dog: { ...initData, x: windowWidth / 2 - 50, y: windowHeight - 100 },
-  food1: { ...initData, x: windowWidth / 2 - 50 },
-};
+class AutoMovedEntity extends MovedEntity {
+  constructor(
+    public info: Info,
+    public feature: Point & Shape,
+    public step: number,
+    public kind: Kind
+  ) {
+    super(info, feature, step);
+  }
 
-console.log("entry");
+  async move() {
+    this.feature.y += this.step;
 
-Promise.all(
-  Object.keys(data).map(
-    async (key) => await new MovedEntity(key, data[key]).init()
-  )
-).then((entities) => {
-  console.log("start");
-  function render() {
-    console.log("render");
-    // ctx.clearRect(0, 0, windowWidth, windowHeight); //清除整个画布
-    entities.forEach((item) => item.move());
-    requestAnimationFrame(render); //自动重绘
+    if (this.feature.y > windowHeight) {
+      this.feature.y = 0;
+
+      if (this.step < 3) {
+        this.step += 1;
+      }
+    }
+
+    await this.render(this.feature);
+    return this;
+  }
+
+  match(target: Point & Shape) {
+    const { x, y, width, height } = target;
+    if (Math.abs(this.feature.x - x) < 5 && Math.abs(this.feature.y - y) < 20) {
+      this.hit();
+      const { food1 } = getData();
+      this.feature = { ...this.feature, ...food1 };
+    }
+  }
+
+  hit() {
+    // 得分
+    if (this.kind === Kind.Positive) {
+      console.log("得分");
+      WORLD.score++;
+    } else {
+      // 扣分
+      console.log("扣分");
+      WORLD.score -= 10;
+    }
+  }
+}
+
+async function run() {
+  const data = getData();
+
+  const [bg, dog] = await Promise.all(
+    Object.keys({ bg: data.bg, dog: data.dog }).map(
+      async (key) => await new MovedEntity({ name: key }, data[key], 0).init()
+    )
+  );
+
+  const list: AutoMovedEntity[] = [];
+
+  setInterval(async () => {
+    if (list.length > 10) {
+      return;
+    }
+    const data = getData();
+    list.push(
+      await new AutoMovedEntity(
+        { name: "shit" },
+        data.shit,
+        data.shit.step,
+        Kind.Negative
+      ).init()
+    );
+  }, 1000 * 10);
+
+  setInterval(async () => {
+    if (list.length > 10) {
+      return;
+    }
+
+    const data = getData();
+    list.push(
+      await new AutoMovedEntity(
+        { name: "food1" },
+        data.food1,
+        data.food1.step,
+        Kind.Positive
+      ).init()
+    );
+  }, 1000 * 1);
+
+  async function render() {
+    list.map((item) => item.match(dog.feature));
+    Promise.all([bg, dog, ...list].map((item) => item.move()));
+    ctx.font = "20px Georgia";
+    ctx.fillText(String(WORLD.score), 10, 20);
+    requestAnimationFrame(render);
   }
 
   render();
-});
 
-// function renderEach(x, y, x1, y1) {
-//   ctx.clearRect(0, 0, windowWidth, windowHeight); //清除整个画布
-//   ctx.drawImage(bg, 0, 0, windowWidth, windowHeight); //背景图
-//   ctx.drawImage(dog, x, y); //狗狗
-//   ctx.drawImage(food, x1, y1); //食物
-// }
+  wx.onTouchMove((res: any) => {
+    dog.feature.x = res.changedTouches[0].clientX - 50;
+    dog.feature.y = res.changedTouches[0].clientY - 50;
+  }); //狗狗跟随触摸点移动
+}
 
-// wx.onTouchMove((res) => {
-//   dX = res.changedTouches[0].clientX;
-//   dY = res.changedTouches[0].clientY;
-// }); //狗狗跟随触摸点移动
+run();
